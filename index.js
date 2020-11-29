94751f9584198d24615cf2530c70055a00f94835
Paul guadalupe veliz lopez <paulsystem32@gmail.com>
2020-11-28 18:30:06 -0700

    Emitir & Escuchar eventos
    - Se implemento la funcionalidad para emitir y escuchar eventos tanto
    como lado del cliente como de servidor.

--- index.js
+++ index.js
@@ -12,9 +12,16 @@ const io = require('socket.io')(server);
 // Mensajes de sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
     client.on('disconnect', () => { 
         console.log('Cliente desconectado.');
      });
+
+    client.on('mensaje', ( payload ) => {
+        console.log(payload);
+    });
+
+    io.emit('mensaje', { admin: 'Nuevo mensaje' });
 });
 
 // Path publico
